5e3220e36d7f44bf387b50c3fe1c58b82f78c450
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-02 20:17:58 +0530

    connect applicaton to mongodb

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello Express");
+require("dotenv").config();
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 7000
+const { connectDB } = require('./config/database');
+
+
+connectDB();
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("School Management System API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
