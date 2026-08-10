b4288906e712f3ad64b4cb7f05f7542a9c6c797f
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-10 22:20:46 +0530

    Feat: Create profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const profileSchema = new mongoose.Schema(
+  {
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+
+    dateOfBirth: {
+      type: Date,
+    },
+
+    bloodGroup: {
+      type: String,
+      enum: [
+        "A+",
+        "A-",
+        "B+",
+        "B-",
+        "AB+",
+        "AB-",
+        "O+",
+        "O-",
+      ],
+    },
+
+    address: {
+      type: String,
+      trim: true,
+    },
+
+    city: {
+      type: String,
+      trim: true,
+    },
+
+    state: {
+      type: String,
+      trim: true,
+    },
+
+    country: {
+      type: String,
+      trim: true,
+      default: "India",
+    },
+
+    postalCode: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Profile", profileSchema);
