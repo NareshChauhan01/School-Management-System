7d34994e1d2106fea1d56e946acae6bd2bfc8334
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-16 23:47:23 +0530

    Index File

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require('express')
 const app = express();
+
 const PORT = process.env.PORT || 7000
 const { connectDB } = require('./config/database');
 const authRoutes = require('./routes/User')
@@ -14,7 +15,9 @@ app.get("/", (req, res) => {
   res.send("School Management System API is running...");
 });
 
+// Authentication Routes
 app.use('/api/v1/auth', authRoutes)
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 })
