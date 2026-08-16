907ae7f774685a7ae5b8eb24922416ebd0a205d2
Naresh Chauhan <nareshsingh0170@gmail.com>
2026-08-16 23:51:27 +0530

    Setup Login route

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup } = require('../controllers/Auth');
+const { signup, login } = require('../controllers/Auth');
 router.post('/signup', signup);
+router.post('/login', login)
 
 module.exports = router
